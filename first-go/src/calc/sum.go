d25732b4a5cc333916b75708ae0f110b807bdb1c
이준수 <ljs93kr@gmail.com>
2016-09-18 00:25:52 +0900

    go public, private 의 지정 방법

--- first-go/src/calc/sum.go
+++ first-go/src/calc/sum.go
@@ -1,9 +1,18 @@
+// 계산 패키지
 package calc
 
+// 전역 변수도 됨
+var TotalNum int
+
+// 두 정수를 더함
+// 두 줄도 됨
+// 대문자로 시작하면 public : 외부에서 사용할 수 있음
 func Sum(a int, b int) int {
 	return a + b
 }
 
+// 두 정수를 뺌 
+// 소문자로 시작하면 private : 파일 내부에서만 사용이 가능
 func sub(a int, b int) int {
 	return a - b
 }
